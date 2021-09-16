a2bd6003dc1e9a4e05b3e45adbeb3c39c1a868bf
zhang214214 <3431463@qq.com>
2021-09-16 11:17:02 +0800

    Update App.vue

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <template>
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <HelloWorld msg="桃 宝 YYDS"/>
   </div>
 </template>
 
