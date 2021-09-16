2537eac955e064bcbcb8c90cc6df293e35fe2860
zhang214214 <3431463@qq.com>
2021-09-16 11:39:53 +0800

    Update App.vue
    sss

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <template>
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png">
-    <p> YYDS </p>
+    <p class="dd"> 桃 宝 YYDS </p>
   </div>
 </template>
 
@@ -24,5 +24,9 @@ export default {
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+}
+.dd {
+  font-size:50px;
+  color:#ff33cc;
 }
 </style>
